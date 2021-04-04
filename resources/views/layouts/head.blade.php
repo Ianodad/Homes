273ff14ed733bddc3a42fbfe77693416728e70e0
ianodad <ianodad@gmail.com>
2021-04-04 13:59:15 +0300

    setup blade

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,39 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<title>Laravel</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+
+<!-- Styles -->
+<link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" />
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<script src="{{ asset(mix('css/app.css')) }}"></script>
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+
+</style>
